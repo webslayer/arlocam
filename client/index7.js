3435a61dfdb3a773ff82a1148a1a07ed8afde2ef
ipreclude <harshbnshah@gmail.com>
2020-04-02 23:38:42 +0530

    Update index7.js

--- client/index7.js
+++ client/index7.js
@@ -1,4 +1,3 @@
-
   var url = 'site';
 
   
@@ -7,13 +6,14 @@
   fetch(url + '/video', {
   method: 'POST', 
   mode: 'cors', 
-  body: JSON.stringify({email: 'navbahl@hotmail.com', password: 'Harsh@123' }),
+  body: JSON.stringify({'email': 'navbahl@hotmail.com', 'password': 'Harsh@123' }),
   redirect: 'follow',
   headers: new Headers({
       'Accept': 'application/json',
       'Content-Type': 'application/json'
   })
-}).then(function() { console.log(res)})
+}).then(function() { var videoLinkJson = res.values;
+                     var videoLinkArrays = Object.videoLinkJson;
+                     console.log(videoLinkArrays);   
+                        })
   .catch(function(res){ console.log(res) })
-
-
