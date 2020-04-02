44191da270ff7f42cc940bb790d7d9b478a85850
Vignesh Pillay <vignesh.umashankar@gmail.com>
2020-04-03 02:42:08 +0530

    j

--- client/index7.js
+++ client/index7.js
@@ -1,19 +1,22 @@
-  var url = 'http://127.0.0.1:5000';
-
-  
-
-
-  fetch(url + '/video', {
-  method: 'POST', 
-  mode: 'no-cors', 
-  body: JSON.stringify({'email': 'navbahl@hotmail.com', 'password': 'Harsh@123' }),
-  redirect: 'follow',
-  headers: new Headers({
-      'Accept': 'application/json',
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
       'Content-Type': 'application/json'
-  })
-}).then(function() { var videoLinkJson = response.values();
-                     var videoLinkArrays = Object.videoLinkJson;
-                     console.log(videoLinkArrays);   
-                        })
-  .catch(function(res){ console.log(response) })
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return await response.json(); // parses JSON response into native JavaScript objects
+}
+
+postData('http://127.0.0.1:5000/video', {'email': 'navbahl@hotmail.com', 'password': 'Harsh@123' })
+  .then((data) => {
+    console.log(data); // JSON data parsed by `response.json()` call
+  });
