75bb5472a6b86bbb2624bf4a6f4e785ad67f73a4
Vignesh Pillay <vignesh.umashankar@gmail.com>
2020-05-13 17:30:36 +0530

    Update timelapse.py

--- server/timelapse.py
+++ server/timelapse.py
@@ -25,7 +25,7 @@ def create_timelapse(datefrom, dateto):
 
     datefrom = datetime.strptime(datefrom, "%d%m%Y")
     dateto = datetime.strptime(dateto, "%d%m%Y")
-    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    fourcc = cv2.VideoWriter_fourcc(*"H264")
     video = cv2.VideoWriter(f"/tmp/{fname}", fourcc, 20, (1904, 1072))
     count = db.snapshots.find(
         {"created_date": {"$gte": datefrom, "$lt": dateto}}
