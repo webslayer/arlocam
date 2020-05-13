af0071716ebd085d718985cad8df989dc6ae2e9f
Vignesh Pillay <vignesh.umashankar@gmail.com>
2020-05-13 17:33:55 +0530

    Update timelapse.py

--- server/timelapse.py
+++ server/timelapse.py
@@ -21,11 +21,11 @@ def create_timelapse(datefrom, dateto):
     timezone = pytz.timezone("Europe/London")
 
     now = datetime.now(timezone).replace(microsecond=0)
-    fname = f"timelapse-{now}.mp4"
+    fname = f"timelapse-{now}.webm"
 
     datefrom = datetime.strptime(datefrom, "%d%m%Y")
     dateto = datetime.strptime(dateto, "%d%m%Y")
-    fourcc = cv2.VideoWriter_fourcc(*"H264")
+    fourcc = cv2.VideoWriter_fourcc(*"vp80")
     video = cv2.VideoWriter(f"/tmp/{fname}", fourcc, 20, (1904, 1072))
     count = db.snapshots.find(
         {"created_date": {"$gte": datefrom, "$lt": dateto}}
