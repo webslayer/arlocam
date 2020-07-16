c5dc8c77ebdc05af9fd14a0efe23f04003904097
vignejs <vignesh.umashankar@gmail.com>
2020-07-16 15:57:40 +0530

    bug fix

--- server/worker.py
+++ server/worker.py
@@ -1,10 +1,8 @@
 import os
-import time
 
 import redis
 from rq import Connection, Queue, Worker
 
-from .app import resume
 
 listen = ["default"]
 
@@ -13,10 +11,6 @@ redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 conn = redis.from_url(redis_url)
 
 if __name__ == "__main__":
-
-    # resume previous scheduler
-    time.sleep(1)
-    resume()
 
     with Connection(conn):
         worker = Worker(list(map(Queue, listen)))
